10475ac1ad0b5437ecf663395fe4f03da1512b33
odbaeu <webmaster@odba.eu>
2017-02-13 03:50:56 +0100

    Initial commit

---
+++ oracle_exporter.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/mattn/go-oci8"
+	"github.com/odbaeu/oracle_exporter/collector"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/version"
+)
+
+var (
+	showVersion = flag.Bool(
+		"version", false,
+		"Print version information.",
+	)
+	listenAddress = flag.String(
+		"web.listen-address", ":9167",
+		"Address to listen on for web interface and telemetry.",
+	)
+	metricPath = flag.String(
+		"web.telemetry-path", "/metrics",
+		"Path under which to expose metrics.",
+	)
+	collectInstSession = flag.Bool(
+		"collect.inst.user_session_count", true,
+		"test",
+	)
+)
+
+// Metric name parts.
+const (
+	// Namespace for all metrics.
+	namespace = "oracle"
+	// Subsystem(s).
+	exporter = "exporter"
+)
+
+// SQL Queries.
+const (
+	// sessionSettingsQuery = `SET SESSION log_slow_filter = 'tmp_table_on_disk,filesort_on_disk'`
+	upQuery = `SELECT status FROM v$instance`
+)
+
+// landingPage contains the HTML served at '/'.
+// TODO: Make this nicer and more informative.
+var landingPage = []byte(`<html>
+<head><title>MySQLd exporter</title></head>
+<body>
+<h1>MySQLd exporter</h1>
+<p><a href='` + *metricPath + `'>Metrics</a></p>
+</body>
+</html>
+`)
+
+// Exporter collects Oracle metrics. It implements prometheus.Collector.
+type Exporter struct {
+	dsn             string
+	duration, error prometheus.Gauge
+	totalScrapes    prometheus.Counter
+	scrapeErrors    *prometheus.CounterVec
+	oracleUp        prometheus.Gauge
+}
+
+// NewExporter returns a new Oracle exporter for the provided DSN.
+func NewExporter(dsn string) *Exporter {
+	return &Exporter{
+		dsn: dsn,
+		duration: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: exporter,
+			Name:      "last_scrape_duration_seconds",
+			Help:      "Duration of the last scrape of metrics from Oracle.",
+		}),
+		totalScrapes: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: exporter,
+			Name:      "scrapes_total",
+			Help:      "Total number of times Oracle was scraped for metrics.",
+		}),
+		scrapeErrors: prometheus.NewCounterVec(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: exporter,
+			Name:      "scrape_errors_total",
+			Help:      "Total number of times an error occurred scraping a Oracle.",
+		}, []string{"collector"}),
+		error: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: exporter,
+			Name:      "last_scrape_error",
+			Help:      "Whether the last scrape of metrics from Oracle resulted in an error (1 for error, 0 for success).",
+		}),
+		oracleUp: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "up",
+			Help:      "Whether the OracleSQL server is up.",
+		}),
+	}
+}
+
+// Describe implements prometheus.Collector.
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	// We cannot know in advance what metrics the exporter will generate
+	// from MySQL. So we use the poor man's describe method: Run a collect
+	// and send the descriptors of all the collected metrics. The problem
+	// here is that we need to connect to the MySQL DB. If it is currently
+	// unavailable, the descriptors will be incomplete. Since this is a
+	// stand-alone exporter and not used as a library within other code
+	// implementing additional metrics, the worst that can happen is that we
+	// don't detect inconsistent metrics created by this exporter
+	// itself. Also, a change in the monitored MySQL instance may change the
+	// exported metrics during the runtime of the exporter.
+
+	metricCh := make(chan prometheus.Metric)
+	doneCh := make(chan struct{})
+
+	go func() {
+		for m := range metricCh {
+			ch <- m.Desc()
+		}
+		close(doneCh)
+	}()
+
+	e.Collect(metricCh)
+	close(metricCh)
+	<-doneCh
+}
+
+// Collect implements prometheus.Collector.
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	e.scrape(ch)
+
+	ch <- e.duration
+	ch <- e.totalScrapes
+	ch <- e.error
+	e.scrapeErrors.Collect(ch)
+	ch <- e.oracleUp
+}
+
+func (e *Exporter) scrape(ch chan<- prometheus.Metric) {
+	e.totalScrapes.Inc()
+	var err error
+	defer func(begun time.Time) {
+		e.duration.Set(time.Since(begun).Seconds())
+		if err == nil {
+			e.error.Set(0)
+		} else {
+			e.error.Set(1)
+		}
+	}(time.Now())
+
+	db, err := sql.Open("oci8", e.dsn)
+	if err != nil {
+		log.Errorln("Error opening connection to database:", err)
+		return
+	}
+	defer db.Close()
+
+	isUpRows, err := db.Query(upQuery)
+	if err != nil {
+		log.Errorln("Error pinging oracle:", err)
+		e.oracleUp.Set(0)
+		return
+	}
+	isUpRows.Close()
+
+	e.oracleUp.Set(1)
+
+	if *collectInstSession {
+		if err = collector.ScrapeUserSessionCount(db, ch); err != nil {
+			log.Errorln("Error scraping for collect.inst.session:", err)
+			e.scrapeErrors.WithLabelValues("collect.inst.session").Inc()
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Fprintln(os.Stdout, version.Print("oracle_exporter"))
+		os.Exit(0)
+	}
+
+	log.Infoln("Starting oracle_exporter", version.Info())
+	log.Infoln("Build context", version.BuildContext())
+
+	dsn := os.Getenv("DATA_SOURCE_NAME")
+	// if len(dsn) == 0 {
+	// 	var err error
+	// 	if dsn, err = parseMycnf(*configMycnf); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
+
+	exporter := NewExporter(dsn)
+	prometheus.MustRegister(exporter)
+
+	http.Handle(*metricPath, prometheus.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(landingPage)
+	})
+
+	log.Infoln("Listening on", *listenAddress)
+	log.Fatal(http.ListenAndServe(*listenAddress, nil))
+}
